713b02fadc54bcf4c8014fc6939942445841bb74
Leticia Medeiros <leticiamedeiroscosta31@Gmail.com>
2025-06-29 00:00:29 -0300

    atualização

--- script.js
+++ script.js
@@ -4,23 +4,37 @@ let container = document.querySelector('.container')
 let items = document.querySelectorAll('.list .item')
 let indicators = document.querySelector('.indicators')
 let dots = indicators.querySelectorAll('ul li')
+let list = container.querySelector ('.list')
 
 let active = 0
 let firstPosition = 0
 let lastPosition = items.length - 1
 
-nextButton.onclick = () => {
+function setSlider() {
     let itemOld = container.querySelector('.list .item.active')
     itemOld.classList.remove('active')
 
+    let dotsOld = indicators.querySelector('ul li.active')
+    dotsOld.classList.remove('active')
+
+    dots[active].classList.add('active')
+    /*marcadores de navegação*/
+
+    indicators.querySelector('.number').innerHTML = '0' + (active + 1) //innerText = texto interno ou innerHTML = HTML interno 
+}
+
+nextButton.onclick = () => {
+    list.style.setProperty('--calculation', -1)
     active = active + 1 > lastPosition ? 0 : active + 1
+    setSlider()
     items[active].classList.add('active')
+    /*setas*/
 }
 
 prevButton.onclick = () => {
-     let itemOld = container.querySelector('.list .item.active')
-    itemOld.classList.remove('active')
-
     active = active - 1 < firstPosition ? lastPosition : active - 1
+     setSlider()
     items[active].classList.add('active')
+
+
 }
